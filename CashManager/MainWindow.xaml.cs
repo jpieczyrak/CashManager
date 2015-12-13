c2dcd3b0490bbe7423dd4f8ebae8195c62bb085e
Jejek <jedrekpieczy@gmail.com>
2015-12-13 19:06:29 +0100

    Project setup

---
+++ CashManager/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace CashManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title += " " + Assembly.GetExecutingAssembly().GetName().Version;
+        }
+    }
+}
