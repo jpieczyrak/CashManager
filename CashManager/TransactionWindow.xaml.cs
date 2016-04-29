e8ad844792ca0e8d4eac5b2eb9f0b6ea37b64517
Jejek <jedrekpieczy@gmail.com>
2016-04-29 20:10:56 +0200

    Transaction view/edit window setup

---
+++ CashManager/TransactionWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace CashManager
+{
+    /// <summary>
+    /// Interaction logic for TransactionWindow.xaml
+    /// </summary>
+    public partial class TransactionWindow : Window
+    {
+        public TransactionWindow()
+        {
+            InitializeComponent();
+
+            //TODO: make copy of actual transaction
+        }
+
+        private void buttonOK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: restore transaction from copy
+            //Close();
+        }
+    }
+}
