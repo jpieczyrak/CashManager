4d7ff37664509613c7670992d3547fc3d3a4a136
Jejek <jedrekpieczy@gmail.com>
2016-04-29 20:10:30 +0200

    Wallet idea added

---
+++ Logic/Wallet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Logic.StocksManagement;
+
+namespace Logic
+{
+    public class Wallet
+    {
+        public List<Stock> Stocks { get; } = new List<Stock>();
+
+        public Wallet()
+        {
+        }
+
+        public void AddStock(Stock stock)
+        {
+            Stocks.Add(stock);
+        }
+    }
+}
