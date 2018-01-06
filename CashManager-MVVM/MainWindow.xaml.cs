b196947908e42d47aeb632d06702ea51c109d888
Jejek <jedrekpieczy@gmail.com>
2018-01-06 19:12:30 +0100

    Added new project for mvvm approach

---
+++ CashManager-MVVM/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using CashManager_MVVM.ViewModel;
+
+namespace CashManager_MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
---
+++ CashManager-MVVM/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace CashManager_MVVM.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
