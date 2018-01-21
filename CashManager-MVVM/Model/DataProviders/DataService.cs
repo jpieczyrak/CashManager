a85bad25bd3d667261d1decf42889a264898ac13
Jejek <jedrekpieczy@gmail.com>
2018-01-21 15:17:26 +0100

    Added base categories

--- CashManager-MVVM/Model/DataProviders/DataService.cs
+++ CashManager-MVVM/Model/DataProviders/DataService.cs
@@ -71,8 +71,27 @@ namespace CashManager_MVVM.Model.DataProviders
 #if DEBUG
             if (categories == null || !categories.Any())
             {
+                var root = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Root" };
+                var home = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Home", Parent = root };
+                var fun = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Fun", Parent = root };
+                var fun_PC = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "PC", Parent = fun };
+                var fun_books = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Books", Parent = fun };
+                var fun_games = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Games", Parent = fun };
+                var fun_games_strategy = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Strategy", Parent = fun_games };
+                var fun_games_fps = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "FPS", Parent = fun_games };
+                var home_cleaning = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Cleaning", Parent = home };
+                var home_food = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Food", Parent = home };
+                var home_food_base = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Base food", Parent = home_food };
+                var home_food_chocolates = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Chocolates", Parent = home_food };
+                var home_food_tea = new Logic.DTO.Category { Id = Guid.NewGuid(), Value = "Tea", Parent = home_food };
                 var dtoCategories = new List<Logic.DTO.Category>
                 {
+                    root,
+                    home, fun,
+                    fun_PC, fun_books, fun_games,
+                    fun_games_strategy, fun_games_fps, 
+                    home_cleaning, home_food,
+                    home_food_base, home_food_chocolates, home_food_tea
 
                 };
                 categories = dtoCategories.Select(Mapper.Map<Category>);
