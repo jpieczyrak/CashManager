44a52deffaf9ff0daabfcceec3aaca7e4675ee4f
Jejek <jedrekpieczy@gmail.com>
2018-11-28 20:12:53 +0100

    Upsert categories command tests

---
+++ CashManager.Infrastructure/Command/Categories/UpsertCategoriesCommand.cs
@@ -0,0 +1,14 @@
+﻿using CashManager.Data.DTO;
+
+namespace CashManager.Infrastructure.Command.Categories
+{
+    public class UpsertCategoriesCommand : ICommand
+    {
+        public Category[] Categories { get; }
+
+        public UpsertCategoriesCommand(Category[] categories)
+        {
+            Categories = categories;
+        }
+    }
+}
