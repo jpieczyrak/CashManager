5c6ab41b294a672eab53facc75697806b806e37b
Jejek <jedrekpieczy@gmail.com>
2018-11-28 20:08:48 +0100

    Upsert tags command tests

---
+++ CashManager.Infrastructure/Command/Tags/UpsertTagsCommand.cs
@@ -0,0 +1,14 @@
+﻿using CashManager.Data.DTO;
+
+namespace CashManager.Infrastructure.Command.Tags
+{
+    public class UpsertTagsCommand : ICommand
+    {
+        public Tag[] Tags { get; }
+
+        public UpsertTagsCommand(Tag[] tags)
+        {
+            Tags = tags;
+        }
+    }
+}
---
+++ CashManager.Infrastructure/Command/Tags/UpsertTagsCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using CashManager.Infrastructure.DbConnection;
+
+using LiteDB;
+
+namespace CashManager.Infrastructure.Command.Tags
+{
+    public class UpsertTagsCommandHandler : ICommandHandler<UpsertTagsCommand>
+    {
+        private readonly LiteDatabase _db;
+
+        public UpsertTagsCommandHandler(LiteRepository repository)
+        {
+            _db = repository.Database;
+        }
+
+        public void Execute(UpsertTagsCommand command)
+        {
+            _db.UpsertBulk(command.Tags);
+        }
+    }
+}
