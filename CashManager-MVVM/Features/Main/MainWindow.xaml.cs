3fa8c0fa4f398f7ac24a16d1bf705eefbd9c508c
Jejek <jedrekpieczy@gmail.com>
2018-11-29 19:22:56 +0100

    Added intellisense to views

--- CashManager-MVVM/Features/Main/MainWindow.xaml.cs
+++ CashManager-MVVM/Features/Main/MainWindow.xaml.cs
@@ -3,14 +3,8 @@ using System.Windows;
 
 namespace CashManager_MVVM.Features.Main
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Initializes a new instance of the MainWindow class.
-        /// </summary>
         public MainWindow(ApplicationViewModel viewModel)
 		{
 			DataContext = viewModel;
