7c440cac4deb899e32c7ad8749ca989e2d387b34
Jejek <jedrekpieczy@gmail.com>
2018-12-01 00:44:16 +0100

    Cleanup

--- CashManager-MVVM/App.xaml.cs
+++ CashManager-MVVM/App.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 
 using Autofac;
 
-using AutoMapper;
-
-using CashManager.Data;
-using CashManager.Data.DTO;
-using CashManager.Infrastructure.Command;
-using CashManager.Infrastructure.Command.NoCommands;
 using CashManager.Infrastructure.Modules;
-using CashManager.Infrastructure.Query;
-using CashManager.Infrastructure.Query.NoQueries;
-using CashManager.Infrastructure.Query.Stocks;
 
 using CashManager_MVVM.Features;
 using CashManager_MVVM.Features.Main;
@@ -22,11 +11,6 @@ using CashManager_MVVM.Temps;
 
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Threading;
-
-using DtoPaymentValue = CashManager.Data.DTO.PaymentValue;
-using DtoCategory = CashManager.Data.DTO.Category;
-using DtoTransaction = CashManager.Data.DTO.Transaction;
-using DtoStock = CashManager.Data.DTO.Stock;
 
 namespace CashManager_MVVM
 {
@@ -48,7 +32,6 @@ namespace CashManager_MVVM
 			var builder = ContainerBuilder();
 
 			var container = builder.Build(); //it could be using, but then there is problem with resolving func factory... anyway it will die with app.
-			TestCommands(container); //todo: remove after test / commands real implementation
 		    DbFiller.Fill(container);
 			container.Resolve<MainWindow>().Show();
 		}
@@ -79,16 +62,5 @@ namespace CashManager_MVVM
 
             return builder;
 		}
-
-		private static void TestCommands(IContainer container)
-		{
-			var commandDispatcher = container.Resolve<ICommandDispatcher>();
-			commandDispatcher.Execute(new NoCommand());
-
-			var queryDispatcher = container.Resolve<IQueryDispatcher>();
-			var test = queryDispatcher.Execute<NoQuery, IEnumerable<string>>(new NoQuery());
-
-		    var test1 = queryDispatcher.Execute<StockQuery, Stock[]>(new StockQuery());
-        }
 	}
 }
