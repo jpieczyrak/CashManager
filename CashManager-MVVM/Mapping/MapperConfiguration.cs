39e108dad01429ff4e4de6b81cafa089d0b55cc2
Jejek <jedrekpieczy@gmail.com>
2018-12-05 21:43:14 +0100

    Fixed automapper issue for multithread testing

--- CashManager-MVVM/Mapping/MapperConfiguration.cs
+++ CashManager-MVVM/Mapping/MapperConfiguration.cs
@@ -10,34 +10,44 @@ namespace CashManager_MVVM.Mapping
 {
     public class MapperConfiguration
     {
+        private static readonly object _lock = new object();
+        private static bool _isInitialized;
+
         public static void Configure()
         {
-            Mapper.Initialize(config =>
+            lock (_lock)
             {
-                config.CreateMap<Category, CashManager.Data.DTO.Category>();
-                config.CreateMap<CashManager.Data.DTO.Category, Category>();
+                if (!_isInitialized)
+                {
+                    Mapper.Initialize(config =>
+                    {
+                        config.CreateMap<Category, CashManager.Data.DTO.Category>();
+                        config.CreateMap<CashManager.Data.DTO.Category, Category>();
 
-                config.CreateMap<Balance, CashManager.Data.DTO.Balance>();
-                config.CreateMap<CashManager.Data.DTO.Balance, Balance>();
+                        config.CreateMap<Balance, CashManager.Data.DTO.Balance>();
+                        config.CreateMap<CashManager.Data.DTO.Balance, Balance>();
 
-                config.CreateMap<Stock, CashManager.Data.DTO.Stock>();
-                config.CreateMap<CashManager.Data.DTO.Stock, Stock>();
+                        config.CreateMap<Stock, CashManager.Data.DTO.Stock>();
+                        config.CreateMap<CashManager.Data.DTO.Stock, Stock>();
 
-                config.CreateMap<Tag, CashManager.Data.DTO.Tag>();
-                config.CreateMap<CashManager.Data.DTO.Tag, Tag>();
+                        config.CreateMap<Tag, CashManager.Data.DTO.Tag>();
+                        config.CreateMap<CashManager.Data.DTO.Tag, Tag>();
 
-                config.CreateMap<PaymentValue, CashManager.Data.DTO.PaymentValue>();
-                config.CreateMap<CashManager.Data.DTO.PaymentValue, PaymentValue>();
+                        config.CreateMap<PaymentValue, CashManager.Data.DTO.PaymentValue>();
+                        config.CreateMap<CashManager.Data.DTO.PaymentValue, PaymentValue>();
 
-                config.CreateMap<TransactionType, CashManager.Data.DTO.TransactionType>();
-                config.CreateMap<CashManager.Data.DTO.TransactionType, TransactionType>();
+                        config.CreateMap<TransactionType, CashManager.Data.DTO.TransactionType>();
+                        config.CreateMap<CashManager.Data.DTO.TransactionType, TransactionType>();
 
-                config.CreateMap<Position, CashManager.Data.DTO.Position>().ConstructUsing(x => new CashManager.Data.DTO.Position());
-                config.CreateMap<CashManager.Data.DTO.Position, Position>();
+                        config.CreateMap<Position, CashManager.Data.DTO.Position>().ConstructUsing(x => new CashManager.Data.DTO.Position());
+                        config.CreateMap<CashManager.Data.DTO.Position, Position>();
 
-                config.CreateMap<Transaction, CashManager.Data.DTO.Transaction>();
-                config.CreateMap<CashManager.Data.DTO.Transaction, Transaction>();
-            });
+                        config.CreateMap<Transaction, CashManager.Data.DTO.Transaction>();
+                        config.CreateMap<CashManager.Data.DTO.Transaction, Transaction>();
+                    });
+                    _isInitialized = true;
+                }
+            }
         }
     }
 }
