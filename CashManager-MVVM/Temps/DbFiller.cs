766f96d2621ca3fc25c5525f44830f87749c2003
Jejek <jedrekpieczy@gmail.com>
2018-12-09 01:43:25 +0100

    Default data provider

--- CashManager-MVVM/Temps/DbFiller.cs
+++ CashManager-MVVM/Temps/DbFiller.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 using Autofac;
 
-using CashManager.Data.DTO;
 using CashManager.Infrastructure.Command;
 using CashManager.Infrastructure.Command.Categories;
 using CashManager.Infrastructure.Command.Stocks;
@@ -13,6 +10,7 @@ using CashManager.Infrastructure.Command.Transactions;
 using CashManager.Infrastructure.Command.TransactionTypes;
 using CashManager.Infrastructure.Query;
 using CashManager.Infrastructure.Query.Transactions;
+using CashManager.Logic.DefaultData;
 
 using DtoPaymentValue = CashManager.Data.DTO.PaymentValue;
 using DtoCategory = CashManager.Data.DTO.Category;
@@ -31,11 +29,11 @@ namespace CashManager_MVVM.Temps
 
             if (!queryDispatcher.Execute<TransactionQuery, DtoTransaction[]>(new TransactionQuery()).Any())
             {
-                var stocks = GetStocks();
-                var categories = GetCategories();
-                var types = GetTransactionTypes();
-                var tags = GetTags();
-                var transactions = GetTransactions(stocks, categories, types, tags);
+                var stocks = DefaultDataProvider.GetStocks();
+                var categories = DefaultDataProvider.GetCategories();
+                var types = DefaultDataProvider.GetTransactionTypes();
+                var tags = DefaultDataProvider.GetTags();
+                var transactions = DefaultDataProvider.GetTransactions(stocks, categories, types, tags);
                 var positions = transactions.SelectMany(x => x.Positions).ToArray();
 
                 commandDispatcher.Execute(new UpsertStocksCommand(stocks));
@@ -45,132 +43,6 @@ namespace CashManager_MVVM.Temps
                 commandDispatcher.Execute(new UpsertTransactionsCommand(transactions));
             }
 #endif
-        }
-
-        private static Transaction[] GetTransactions(Stock[] stocks, Category[] categories, TransactionType[] types, Tag[] tags)
-        {
-            var dtoTransactions = new[]
-            {
-                new DtoTransaction(types[1], DateTime.Now.AddDays(-45), "title 1 - buying some stuff", "notes 1", new List<Position>
-                    {
-                        new Position
-                        {
-                            Category = categories.FirstOrDefault(x => x.Parent == null),
-                            Value = new DtoPaymentValue { TaxPercentValue = 8, GrossValue = 10},
-                            Title = "my position 1 - apples",
-                            Tags = new List<Tag> { tags[0] }
-                        },
-                        new Position
-                        {
-                            Category = categories.FirstOrDefault(x => x.Parent != null),
-                            Value = new DtoPaymentValue {  TaxPercentValue = 5, GrossValue = 15 },
-                            Title = "my position 2 - tea",
-                            Tags = new List<Tag> { tags[1] }
-                        }
-                    },
-                    stocks[0], stocks[2], "inputsource1"),
-                new DtoTransaction(types[0], DateTime.Now.AddDays(-30), "title 2 - work", "notes 2", new List<Position>
-                    {
-                        new Position
-                        {
-                            Category = categories.Skip(5).FirstOrDefault(x => x.Parent != null),
-                            Value = new DtoPaymentValue { TaxPercentValue = 23, NetValue = 1000 },
-                            Title = "income",
-                            Tags = new List<Tag> { tags[0], tags[2] }
-                        }
-                    }, stocks[0], stocks[3], "inputsource2"),
-                new DtoTransaction(types[1], DateTime.Now.AddDays(-20).AddHours(12), "title 3 - buying more stuff", "stuff!!!", new List<Position>
-                    {
-                        new Position
-                        {
-                            Category = categories.FirstOrDefault(x => x.Parent == null),
-                            Value = new DtoPaymentValue { TaxPercentValue = 23, GrossValue = 2499 },
-                            Title = "sth expensive",
-                            Tags = new List<Tag> { tags[0] }
-                        },
-                    },
-                    stocks[1], stocks[3], "inputsource3"),
-                new DtoTransaction(types[1], DateTime.Now, "title 4 - buying even more stuff", "stuff, stuff!!!", new List<Position>
-                    {
-                        new Position
-                        {
-                            Category = categories.FirstOrDefault(x => x.Parent == null),
-                            Value = new DtoPaymentValue { TaxPercentValue = 23, NetValue = 7129 },
-                            Title = "sth expensive 2",
-                            Tags = new List<Tag> { tags[1], tags[2] }
-                        },
-                    },
-                    stocks[1], stocks[2], "inputsource4"),
-            };
-
-            return dtoTransactions;
-        }
-
-        private static DtoCategory[] GetCategories()
-        {
-            var root = new DtoCategory { Name = "Root" };
-            var home = new DtoCategory { Name = "Home", Parent = root };
-            var fun = new DtoCategory { Name = "Fun", Parent = root };
-            var fun_PC = new DtoCategory { Name = "PC", Parent = fun };
-            var fun_books = new DtoCategory { Name = "Books", Parent = fun };
-            var fun_games = new DtoCategory { Name = "Games", Parent = fun };
-            var fun_games_strategy = new DtoCategory { Name = "Strategy", Parent = fun_games };
-            var fun_games_fps = new DtoCategory { Name = "FPS", Parent = fun_games };
-            var home_cleaning = new DtoCategory { Name = "Cleaning", Parent = home };
-            var home_food = new DtoCategory { Name = "Food", Parent = home };
-            var home_food_base = new DtoCategory { Name = "Base food", Parent = home_food };
-            var home_food_chocolates = new DtoCategory { Name = "Chocolates", Parent = home_food };
-            var home_food_tea = new DtoCategory { Name = "Tea", Parent = home_food };
-            var dtoCategories = new[]
-            {
-                root,
-                home,
-                fun,
-                fun_PC,
-                fun_books,
-                fun_games,
-                fun_games_strategy,
-                fun_games_fps,
-                home_cleaning,
-                home_food,
-                home_food_base,
-                home_food_chocolates,
-                home_food_tea
-            };
-
-            return dtoCategories;
-        }
-
-        private static DtoStock[] GetStocks()
-        {
-            return new[]
-            {
-                new DtoStock { Name = "User1", IsUserStock = true, Balance = new Balance(DateTime.MinValue, 100)},
-                new DtoStock { Name = "Wallet", IsUserStock = true, Balance = new Balance(DateTime.MinValue, 1048576) },
-                new DtoStock { Name = "Ex1" },
-                new DtoStock { Name = "Ex2" }
-            };
-        }
-
-        private static TransactionType[] GetTransactionTypes()
-        {
-            return new[]
-            {
-                new TransactionType { Income = true, Name = "Work", IsDefault = true },
-                new TransactionType { Outcome = true, Name = "Buy", IsDefault = true },
-                new TransactionType { Name = "Transfer" },
-                new TransactionType { Income = true, Name = "Gifts" }
-            };
-        }
-
-        private static Tag[] GetTags()
-        {
-            return new Tag[]
-            {
-                new Tag { Name = "tag 1" }, 
-                new Tag { Name = "tag 2" }, 
-                new Tag { Name = "tag 3" }, 
-            };
         }
     }
 }
---
+++ CashManager.Logic/DefaultData/DefaultDataProvider.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CashManager.Data.DTO;
+
+namespace CashManager.Logic.DefaultData
+{
+    public static class DefaultDataProvider
+    {
+        public static Transaction[] GetTransactions(Stock[] stocks, Category[] categories, TransactionType[] types, Tag[] tags)
+        {
+            var dtoTransactions = new[]
+            {
+                new Transaction(types[1], DateTime.Now.AddDays(-45), "title 1 - buying some stuff", "notes 1", new List<Position>
+                    {
+                        new Position
+                        {
+                            Category = categories.FirstOrDefault(x => x.Parent == null),
+                            Value = new PaymentValue { TaxPercentValue = 8, GrossValue = 10},
+                            Title = "my position 1 - apples",
+                            Tags = new List<Tag> { tags[0] }
+                        },
+                        new Position
+                        {
+                            Category = categories.FirstOrDefault(x => x.Parent != null),
+                            Value = new PaymentValue {  TaxPercentValue = 5, GrossValue = 15 },
+                            Title = "my position 2 - tea",
+                            Tags = new List<Tag> { tags[1] }
+                        }
+                    },
+                    stocks[0], stocks[2], "inputsource1"),
+                new Transaction(types[0], DateTime.Now.AddDays(-30), "title 2 - work", "notes 2", new List<Position>
+                    {
+                        new Position
+                        {
+                            Category = categories.Skip(5).FirstOrDefault(x => x.Parent != null),
+                            Value = new PaymentValue { TaxPercentValue = 23, NetValue = 1000 },
+                            Title = "income",
+                            Tags = new List<Tag> { tags[0], tags[2] }
+                        }
+                    }, stocks[0], stocks[3], "inputsource2"),
+                new Transaction(types[1], DateTime.Now.AddDays(-20).AddHours(12), "title 3 - buying more stuff", "stuff!!!", new List<Position>
+                    {
+                        new Position
+                        {
+                            Category = categories.FirstOrDefault(x => x.Parent == null),
+                            Value = new PaymentValue { TaxPercentValue = 23, GrossValue = 2499 },
+                            Title = "sth expensive",
+                            Tags = new List<Tag> { tags[0] }
+                        },
+                    },
+                    stocks[1], stocks[3], "inputsource3"),
+                new Transaction(types[1], DateTime.Now, "title 4 - buying even more stuff", "stuff, stuff!!!", new List<Position>
+                    {
+                        new Position
+                        {
+                            Category = categories.FirstOrDefault(x => x.Parent == null),
+                            Value = new PaymentValue { TaxPercentValue = 23, NetValue = 7129 },
+                            Title = "sth expensive 2",
+                            Tags = new List<Tag> { tags[1], tags[2] }
+                        },
+                    },
+                    stocks[1], stocks[2], "inputsource4"),
+            };
+
+            return dtoTransactions;
+        }
+
+        public static Category[] GetCategories()
+        {
+            var root = new Category { Name = "Root" };
+            var home = new Category { Name = "Home", Parent = root };
+            var fun = new Category { Name = "Fun", Parent = root };
+            var fun_PC = new Category { Name = "PC", Parent = fun };
+            var fun_books = new Category { Name = "Books", Parent = fun };
+            var fun_games = new Category { Name = "Games", Parent = fun };
+            var fun_games_strategy = new Category { Name = "Strategy", Parent = fun_games };
+            var fun_games_fps = new Category { Name = "FPS", Parent = fun_games };
+            var home_cleaning = new Category { Name = "Cleaning", Parent = home };
+            var home_food = new Category { Name = "Food", Parent = home };
+            var home_food_base = new Category { Name = "Base food", Parent = home_food };
+            var home_food_chocolates = new Category { Name = "Chocolates", Parent = home_food };
+            var home_food_tea = new Category { Name = "Tea", Parent = home_food };
+            var dtoCategories = new[]
+            {
+                root,
+                home,
+                fun,
+                fun_PC,
+                fun_books,
+                fun_games,
+                fun_games_strategy,
+                fun_games_fps,
+                home_cleaning,
+                home_food,
+                home_food_base,
+                home_food_chocolates,
+                home_food_tea
+            };
+
+            return dtoCategories;
+        }
+
+        public static Stock[] GetStocks()
+        {
+            return new[]
+            {
+                new Stock { Name = "User1", IsUserStock = true, Balance = new Balance(DateTime.MinValue, 100)},
+                new Stock { Name = "Wallet", IsUserStock = true, Balance = new Balance(DateTime.MinValue, 1048576) },
+                new Stock { Name = "Ex1" },
+                new Stock { Name = "Ex2" }
+            };
+        }
+
+        public static TransactionType[] GetTransactionTypes()
+        {
+            return new[]
+            {
+                new TransactionType { Income = true, Name = "Work", IsDefault = true },
+                new TransactionType { Outcome = true, Name = "Buy", IsDefault = true },
+                new TransactionType { Name = "Transfer" },
+                new TransactionType { Income = true, Name = "Gifts" }
+            };
+        }
+
+        public static Tag[] GetTags()
+        {
+            return new Tag[]
+            {
+                new Tag { Name = "tag 1" },
+                new Tag { Name = "tag 2" },
+                new Tag { Name = "tag 3" },
+            };
+        }
+    }
+}
