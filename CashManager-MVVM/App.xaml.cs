c38be1b6a9e9afdf9202d2026b57d83455d6f537
Jejek <jedrekpieczy@gmail.com>
2018-12-09 01:59:29 +0100

    Extracted DI configuration

--- CashManager-MVVM/App.xaml.cs
+++ CashManager-MVVM/App.xaml.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
 using Autofac;
 
-using CashManager.Infrastructure.Modules;
-
-using CashManager_MVVM.Features;
+using CashManager_MVVM.DI;
 using CashManager_MVVM.Features.Main;
 using CashManager_MVVM.Temps;
 
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Threading;
 
 namespace CashManager_MVVM
@@ -29,38 +25,11 @@ namespace CashManager_MVVM
 		{
 			base.OnStartup(e);
 
-			var builder = ContainerBuilder();
+			var builder = AutofacConfiguration.ContainerBuilder();
 
 			var container = builder.Build(); //it could be using, but then there is problem with resolving func factory... anyway it will die with app.
 		    DbFiller.Fill(container);
 			container.Resolve<MainWindow>().Show();
-		}
-
-	    private static ContainerBuilder ContainerBuilder()
-		{
-			var builder = new ContainerBuilder();
-			builder.RegisterAssemblyModules(typeof(DatabaseCommunicationModule).Assembly);
-			builder.RegisterType<MainWindow>();
-
-            builder.RegisterAssemblyTypes(typeof(ApplicationViewModel).Assembly)
-				   .Where(t => t.IsSubclassOf(typeof(ViewModelBase)) && !string.Equals(t.Name, nameof(ApplicationViewModel)))
-				   .Named<ViewModelBase>(x => x.Name)
-				   .As(t => t);
-		    builder.RegisterType<ApplicationViewModel>()
-		           .As<ApplicationViewModel>()
-		           .Named<ViewModelBase>(nameof(ApplicationViewModel))
-		           .SingleInstance()
-		           .ExternallyOwned();
-
-            builder.RegisterType<ViewModelFactory>().As<ViewModelFactory>();
-
-			builder.Register<Func<Type, ViewModelBase>>(c =>
-			{
-				var context = c.Resolve<IComponentContext>();
-                return type => context.ResolveNamed<ViewModelBase>(type.Name);
-			});
-
-            return builder;
 		}
 	}
 }
---
+++ CashManager-MVVM/DI/AutofacConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Autofac;
+
+using CashManager.Infrastructure.Modules;
+
+using CashManager_MVVM.Features;
+using CashManager_MVVM.Features.Main;
+
+using GalaSoft.MvvmLight;
+
+namespace CashManager_MVVM.DI
+{
+    public static class AutofacConfiguration
+    {
+        public static ContainerBuilder ContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(typeof(DatabaseCommunicationModule).Assembly);
+            builder.RegisterType<MainWindow>();
+
+            builder.RegisterAssemblyTypes(typeof(ApplicationViewModel).Assembly)
+                   .Where(t => t.IsSubclassOf(typeof(ViewModelBase)) && !string.Equals(t.Name, nameof(ApplicationViewModel)))
+                   .Named<ViewModelBase>(x => x.Name)
+                   .As(t => t);
+            builder.RegisterType<ApplicationViewModel>()
+                   .As<ApplicationViewModel>()
+                   .Named<ViewModelBase>(nameof(ApplicationViewModel))
+                   .SingleInstance()
+                   .ExternallyOwned();
+
+            builder.RegisterType<ViewModelFactory>().As<ViewModelFactory>();
+
+            builder.Register<Func<Type, ViewModelBase>>(c =>
+            {
+                var context = c.Resolve<IComponentContext>();
+                return type => context.ResolveNamed<ViewModelBase>(type.Name);
+            });
+
+            return builder;
+        }
+    }
+}
