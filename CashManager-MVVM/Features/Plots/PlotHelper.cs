7b7981299f4c24e91c1f6d407b33e009e9e5e424
Jejek <jedrekpieczy@gmail.com>
2019-01-01 16:05:44 +0100

    Fixed app resources in tests

--- CashManager-MVVM/Features/Plots/PlotHelper.cs
+++ CashManager-MVVM/Features/Plots/PlotHelper.cs
@@ -8,9 +8,18 @@ namespace CashManager_MVVM.Features.Plots
     {
         internal static PlotModel CreatePlotModel()
         {
-            var textColor = OxyColor.Parse(Application.Current.Resources["ForegroundColor"].ToString());
-            var borderColor = OxyColor.Parse(Application.Current.Resources["BorderColor"].ToString());
-            var selectColor = OxyColor.Parse(Application.Current.Resources["HoverBorderAccentColor"].ToString());
+            var application = Application.Current;
+
+            var defaultColor = OxyColor.FromRgb(100, 100, 100);
+            var textColor = application != null
+                                ? OxyColor.Parse(application.Resources["ForegroundColor"].ToString())
+                                : defaultColor;
+            var borderColor = application != null
+                                  ? OxyColor.Parse(application.Resources["BorderColor"].ToString())
+                                  : defaultColor;
+            var selectColor = application != null
+                                  ? OxyColor.Parse(application.Resources["HoverBorderAccentColor"].ToString())
+                                  : defaultColor;
 
             return new PlotModel
             {
