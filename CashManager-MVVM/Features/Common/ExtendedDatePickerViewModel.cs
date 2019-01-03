3264306c739b5af1116f3f708620a02ececc990b
Jejek <jedrekpieczy@gmail.com>
2019-01-03 21:48:32 +0100

    Buttons for quick navigation in ex date picker + tooltips

--- CashManager-MVVM/Features/Common/ExtendedDatePickerViewModel.cs
+++ CashManager-MVVM/Features/Common/ExtendedDatePickerViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 
 namespace CashManager_MVVM.Features.Common
@@ -14,8 +16,18 @@ namespace CashManager_MVVM.Features.Common
         public RelayCommand SubtractYearCommand { get; }
 
         public RelayCommand AddDayCommand { get; }
+
         public RelayCommand AddMonthCommand { get; }
+
         public RelayCommand AddYearCommand { get; }
+
+        public RelayCommand FirstDayCommand { get; }
+
+        public RelayCommand LastDayCommand { get; }
+
+        public RelayCommand FirstMonthCommand { get; }
+
+        public RelayCommand LastMonthCommand { get; }
 
         public ExtendedDatePickerViewModel(ExtendedDatePicker extendedDatePicker)
         {
@@ -28,6 +40,12 @@ namespace CashManager_MVVM.Features.Common
             AddDayCommand = new RelayCommand(() => Owner.SelectedValue = Owner.SelectedValue.AddDays(1));
             AddMonthCommand = new RelayCommand(() => Owner.SelectedValue = Owner.SelectedValue.AddMonths(1));
             AddYearCommand = new RelayCommand(() => Owner.SelectedValue = Owner.SelectedValue.AddYears(1));
+
+            FirstDayCommand = new RelayCommand(() => Owner.SelectedValue = new DateTime(Owner.SelectedValue.Year, Owner.SelectedValue.Month, 1));
+            LastDayCommand = new RelayCommand(() => Owner.SelectedValue = new DateTime(Owner.SelectedValue.Year, Owner.SelectedValue.Month, 1).AddMonths(1).AddDays(-1));
+
+            FirstMonthCommand = new RelayCommand(() => Owner.SelectedValue = new DateTime(Owner.SelectedValue.Year, 1, Owner.SelectedValue.Day));
+            LastMonthCommand = new RelayCommand(() => Owner.SelectedValue = new DateTime(Owner.SelectedValue.Year, 12, Owner.SelectedValue.Day));
         }
     }
 }
