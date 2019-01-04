013b941717b8f574f71d29bbb7990595b8a569b7
Jejek <jedrekpieczy@gmail.com>
2019-01-04 20:01:21 +0100

    Added base about window

---
+++ CashManager-MVVM/Features/Main/AboutWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CashManager_MVVM.Features.Main
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
