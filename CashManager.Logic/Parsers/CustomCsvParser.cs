83ff1c36e2bd208673949931a6162f5caf09c959
Jejek <jedrekpieczy@gmail.com>
2019-01-05 21:20:27 +0100

    Custom csv parser #1

---
+++ CashManager.Logic/Parsers/CustomCsvParser.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+
+using CashManager.Data.DTO;
+
+namespace CashManager.Logic.Parsers
+{
+    public class CustomCsvParser : IParser
+    {
+        private readonly object _rules;
+
+        public Balance Balance { get; private set; }
+
+        public CustomCsvParser(Rule[] rules)
+        {
+            _rules = rules;
+        }
+
+        public Transaction[] Parse(string input, Stock userStock, Stock externalStock, TransactionType defaultOutcome,
+            TransactionType defaultIncome)
+        {
+            input = input.Replace("\"", string.Empty);
+            return new Transaction[] { };
+        }
+    }
+
+    public class Rule
+    {
+
+    }
+}
---
+++ CashManager.Tests/Parsers/CustomCsvParserTests.cs
@@ -0,0 +1,24 @@
+﻿using CashManager.Logic.Parsers;
+
+using Xunit;
+
+namespace CashManager.Tests.Parsers
+{
+    public class CustomCsvParserTests : BaseParserTests
+    {
+        [Fact]
+        public void Parse_None_Empty()
+        {
+            //given
+            var rules = new Rule[0];
+            var parser = new CustomCsvParser(rules);
+            var input = "";
+
+            //when
+            var result = parser.Parse(input, null, null, null, null);
+
+            //then
+            Assert.Empty(result);
+        }
+    }
+}
