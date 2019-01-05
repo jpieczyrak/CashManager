052d03ed3df5e7107c9e2dcc6f3bf83cc9886327
Jejek <jedrekpieczy@gmail.com>
2019-01-05 18:22:12 +0100

    Custom csv parser #2

--- CashManager.Tests/Parsers/CustomCsvParserTests.cs
+++ CashManager.Tests/Parsers/CustomCsvParserTests.cs
@@ -7,10 +7,28 @@ namespace CashManager.Tests.Parsers
     public class CustomCsvParserTests : BaseParserTests
     {
         [Fact]
-        public void Parse_None_Empty()
+        public void Parse_NoneRulesEmptyInput_Empty()
         {
             //given
             var rules = new Rule[0];
+            var parser = new CustomCsvParser(rules);
+            var input = "";
+
+            //when
+            var result = parser.Parse(input, null, null, null, null);
+
+            //then
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Parse_SomeRulesEmptyInput_Empty()
+        {
+            //given
+            var rules = new Rule[]
+            {
+                new Rule()
+            };
             var parser = new CustomCsvParser(rules);
             var input = "";
 
