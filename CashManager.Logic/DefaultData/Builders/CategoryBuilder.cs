1875c05e73eff29ad3d56f570f9170a3bc30125e
Jejek <jedrekpieczy@gmail.com>
2019-01-06 17:35:56 +0100

    Category builder #1

---
+++ CashManager.Logic/DefaultData/Builders/CategoryBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using CashManager.Data.DTO;
+
+namespace CashManager.Logic.DefaultData.Builders
+{
+    public class CategoryBuilder
+    {
+        public Category LastCategory { get; private set; }
+
+        public List<Category> Categories { get; }
+
+        public CategoryBuilder()
+        {
+            Categories = new List<Category>();
+        }
+
+        public CategoryBuilder AddCategory(Category category)
+        {
+            LastCategory = category;
+            if (category != null) Categories.Add(category);
+            return this;
+        }
+    }
+}
---
+++ CashManager.Tests/DataBuilderTests/CategoryBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using CashManager.Data.DTO;
+using CashManager.Logic.DefaultData.Builders;
+
+using Xunit;
+
+namespace CashManager.Tests.DataBuilderTests
+{
+    public class CategoryBuilderTests
+    {
+        [Fact]
+        public void AddCategory_Null_NoError()
+        {
+            //given
+            var builder = new CategoryBuilder();
+
+            //when
+            var result = builder.AddCategory(null);
+
+            //then
+            Assert.Null(result.LastCategory);
+            Assert.Empty(result.Categories);
+        }
+
+        [Fact]
+        public void AddCategory_ValidCategory_ProperlySet()
+        {
+            //given
+            var builder = new CategoryBuilder();
+            var category = new Category();
+
+            //when
+            var result = builder.AddCategory(category);
+
+            //then
+            Assert.Equal(category, result.LastCategory);
+            Assert.Contains(category, result.Categories);
+        }
+    }
+}
