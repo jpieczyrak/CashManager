8882baac7539b5567f2f9dc1684c4715e332c2ed
Jejek <jedrekpieczy@gmail.com>
2019-01-11 14:37:50 +0100

    Date range picker control

---
+++ CashManager-MVVM/Features/Common/DateRangePicker.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using CashManager_MVVM.Model.Selectors;
+
+namespace CashManager_MVVM.Features.Common
+{
+    public partial class DateRangePicker : UserControl
+    {
+        private const string DATE_FRAME_PROPERTY_NAME = "DateFrame";
+
+        public static readonly DependencyProperty DateFrameProperty = DependencyProperty.Register(
+            DATE_FRAME_PROPERTY_NAME,
+            typeof(DateFrame),
+            typeof(DateRangePicker));
+
+        public DateFrame DateFrame
+        {
+            get => (DateFrame) GetValue(DateFrameProperty);
+            set => SetValue(DateFrameProperty, value);
+        }
+
+        public DateRangePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
