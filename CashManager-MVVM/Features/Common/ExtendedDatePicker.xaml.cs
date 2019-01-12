c6d220fa15aa911bb5e63805d45969317bb7f6d6
Jejek <jedrekpieczy@gmail.com>
2019-01-12 22:52:13 +0100

    Bindable end date

--- CashManager-MVVM/Features/Common/ExtendedDatePicker.xaml.cs
+++ CashManager-MVVM/Features/Common/ExtendedDatePicker.xaml.cs
@@ -11,10 +11,20 @@ namespace CashManager_MVVM.Features.Common
                 new FrameworkPropertyMetadata(DateTime.Today, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                     OnSelectedValuePropertyChanged));
 
+        public static readonly DependencyProperty EndDateProperty =
+            DependencyProperty.Register(nameof(EndDate), typeof(DateTime), typeof(ExtendedDatePicker),
+                new FrameworkPropertyMetadata(DateTime.Today));
+
         public DateTime SelectedValue
         {
             get => (DateTime) GetValue(SelectedValueProperty);
             set => SetValue(SelectedValueProperty, value); //should update source (by design)
+        }
+
+        public DateTime EndDate
+        {
+            get => (DateTime) GetValue(EndDateProperty);
+            set => SetValue(EndDateProperty, value);
         }
 
         public ExtendedDatePickerViewModel ViewModel { get; private set; }
