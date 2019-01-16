f6c4c46342b1a1c556c432efe495adf8428df04a
Jejek <jedrekpieczy@gmail.com>
2019-01-16 16:58:05 +0100

    Cleanup

--- CashManager-MVVM/CommonData/TransactionsProvider.cs
+++ CashManager-MVVM/CommonData/TransactionsProvider.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 using AutoMapper;
 
 using CashManager.Infrastructure.Query;
-using CashManager.Infrastructure.Query.Stocks;
 using CashManager.Infrastructure.Query.Transactions;
-using CashManager.Infrastructure.Query.TransactionTypes;
 using CashManager.Logic.Wrappers;
 
 using CashManager_MVVM.Model;
 
 using DtoTransaction = CashManager.Data.DTO.Transaction;
-using DtoType = CashManager.Data.DTO.TransactionType;
-using DtoStock = CashManager.Data.DTO.Stock;
 
 namespace CashManager_MVVM.CommonData
 {
@@ -23,12 +18,6 @@ namespace CashManager_MVVM.CommonData
 
         public TransactionsProvider(IQueryDispatcher queryDispatcher)
         {
-            //ensure that types and stocks exists in mapper [after transaction query mapping optimization
-            //such code is needed to prevent empty fields in gui (before first loading of stocks/tt data)]
-            //todo: extract it to separate providers?
-            using (new MeasureTimeWrapper(() => Mapper.Map<TransactionType[]>(queryDispatcher.Execute<TransactionTypesQuery, DtoType[]>(new TransactionTypesQuery())).ToArray(), "query types")) { }
-            using (new MeasureTimeWrapper(() => Mapper.Map<Stock[]>(queryDispatcher.Execute<StockQuery, DtoStock[]>(new StockQuery())), "query stocks")) { }
-
             var query = new TransactionQuery();
             DtoTransaction[] dtos = null;
             using (new MeasureTimeWrapper(() => dtos = queryDispatcher.Execute<TransactionQuery, DtoTransaction[]>(query), "query transactions")) { }
