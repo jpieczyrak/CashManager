7e1d292da86402fa4fba7be2e77a526f39aa295f
Jejek <jedrekpieczy@gmail.com>
2019-01-21 15:32:36 +0100

    MessageBox service base implementation

--- CashManager-MVVM/UserCommunication/MessageBoxService.cs
+++ CashManager-MVVM/UserCommunication/MessageBoxService.cs
@@ -1,10 +1,17 @@
-﻿namespace CashManager_MVVM.UserCommunication
+﻿using System.Windows;
+
+namespace CashManager_MVVM.UserCommunication
 {
     public class MessageBoxService : IMessagesService
     {
+        #region IMessagesService
+
         public bool ShowQuestionMessage(string title = null, string question = null)
         {
-            return false;
+            var result = MessageBox.Show(question, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return result == MessageBoxResult.Yes;
         }
+
+        #endregion
     }
 }
