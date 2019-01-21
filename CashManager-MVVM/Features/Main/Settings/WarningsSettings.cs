8422b80127cba9c708dbe44cfa211bb4fb80296a
Jejek <jedrekpieczy@gmail.com>
2019-01-21 19:35:38 +0100

    Select all warnings checkbox

--- CashManager-MVVM/Features/Main/Settings/WarningsSettings.cs
+++ CashManager-MVVM/Features/Main/Settings/WarningsSettings.cs
@@ -4,10 +4,27 @@ namespace CashManager_MVVM.Features.Main.Settings
 {
     internal class WarningsSettings : ObservableObject
     {
+        public bool? AllSelected
+        {
+            get => QuestionForCategoryDelete;
+            set
+            {
+                if (AllSelected == value) return;
+                QuestionForCategoryDelete = value ?? false;
+                RaisePropertyChanged();
+            }
+        }
+
         public bool QuestionForCategoryDelete
         {
             get => Properties.Settings.Default.QuestionForCategoryDelete;
-            set => Properties.Settings.Default.QuestionForCategoryDelete = value;
+            set
+            {
+                if (Properties.Settings.Default.QuestionForCategoryDelete == value) return;
+                Properties.Settings.Default.QuestionForCategoryDelete = value;
+                RaisePropertyChanged(nameof(QuestionForCategoryDelete));
+                RaisePropertyChanged(nameof(AllSelected));
+            }
         }
 
         internal WarningsSettings() { }
