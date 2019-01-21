e23f4666d77bae7660b107f4b89a5a32b4186927
Jejek <jedrekpieczy@gmail.com>
2019-01-21 15:32:20 +0100

    Warning question

--- CashManager-MVVM/Properties/Settings.Designer.cs
+++ CashManager-MVVM/Properties/Settings.Designer.cs
@@ -130,5 +130,17 @@ namespace CashManager_MVVM.Properties {
                 this["UseExtendedDatePicker"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool QuestionForCategoryDelete {
+            get {
+                return ((bool)(this["QuestionForCategoryDelete"]));
+            }
+            set {
+                this["QuestionForCategoryDelete"] = value;
+            }
+        }
     }
 }
