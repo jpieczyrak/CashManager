52107e3c6022b7261db39b4569b2365b6c313c18
Jejek <jedrekpieczy@gmail.com>
2019-01-22 02:13:20 +0100

    Paymentvalue control

---
+++ CashManager-MVVM/Features/Transactions/PaymentValueControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CashManager_MVVM.Features.Transactions
+{
+    public partial class PaymentValueControl : UserControl
+    {
+        public PaymentValueControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
