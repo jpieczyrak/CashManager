8baff3774f13cbb8fe05da626eb7912bc171aec6
Jejek <jedrekpieczy@gmail.com>
2019-01-22 22:40:07 +0100

    Acceptance tests #4

--- CashManager.Tests/AcceptanceCriteria/Transactions/Edit/TransactionCreateAndStockChangeTests.cs
+++ CashManager.Tests/AcceptanceCriteria/Transactions/Edit/TransactionCreateAndStockChangeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 using Autofac;
 
@@ -8,8 +9,6 @@ using CashManager_MVVM.Features.Main;
 using CashManager_MVVM.Features.Search;
 using CashManager_MVVM.Features.Stocks;
 using CashManager_MVVM.Features.Transactions;
-using CashManager_MVVM.Features.TransactionTypes;
-using CashManager_MVVM.Model;
 
 using Xunit;
 
@@ -21,38 +20,61 @@ namespace CashManager.Tests.AcceptanceCriteria.Transactions.Edit
         public TransactionCreateAndStockChangeTests(CleanableDatabaseFixture fixture) : base(fixture) { }
 
         [Theory]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, 5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, 5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, 5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, -1, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 0, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 1, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, -1, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 0, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 1, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, -1, 5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 0, 5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome,TransactionTypes.Income, 1, 5, 1500)]
-        public void EditingTransactionShouldModifyStockBalance(decimal startBalance, decimal transactionValue, TransactionTypes originalType, TransactionTypes destinationType, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, -1, 0, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 0, 0, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 1, 0, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, -1, -5, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 0, -5, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 1, -5, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, -1, 5, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 0, 5, 1500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Outcome, TransactionTypes.Income, 1, 5, 1500)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, -1, 0, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 0, 0, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 1, 0, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, -1, -5, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 0, -5, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 1, -5, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, -1, 5, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 0, 5, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Income, 1, 5, 2000)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, 0, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, 0, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, 0, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, -5, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, -5, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, -5, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, -1, 5, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 0, 5, 500)]
+        [InlineData(1000, 500, 500, TransactionTypes.Income, TransactionTypes.Outcome, 1, 5, 500)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, -1, 0, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 0, 0, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 1, 0, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, -1, -5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 0, -5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 1, -5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, -1, 5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 0, 5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Outcome, 1, 5, 0)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Income, TransactionTypes.Income, 0, 0, 2000)]
+        [InlineData(1000, 500, 1000, TransactionTypes.Outcome, TransactionTypes.Outcome, 0, 0, 0)]
+        public void EditTransaction_ModifyStock_StockBalanceShouldBeModified(decimal startBalance, decimal transactionValue, decimal newValue, TransactionTypes originalType, TransactionTypes destinationType, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
         {
             //given
             var app = _fixture.Container.Resolve<ApplicationViewModel>();
 
             var descType = CreateType(destinationType);
-            var sourceType = CreateType(originalType);
+            var sourceType = destinationType == originalType ? descType : CreateType(originalType);
+            if (destinationType == originalType && destinationType == TransactionTypes.Outcome)
+                CreateType(TransactionTypes.Income); //there have to be income type to create stock balance
             var userStock = CreateUserStock(startBalance, daysSinceLastStockEdit);
 
             app.SelectViewModelCommand.Execute(ViewModel.Transaction);
-            var transactionVm = (TransactionViewModel) app.SelectedViewModel;
+            var transactionVm = (TransactionViewModel)app.SelectedViewModel;
 
-            transactionVm.Modes[TransactionEditModes.ChangeStockBalance].IsSelected = true;
+            transactionVm.SetUpdateMode(TransactionEditModes.ChangeStockBalance);
             var transaction = transactionVm.Transaction;
+            var id = transaction.Id;
             transaction.Title = "first one unedited";
             transaction.Type = sourceType;
             transaction.UserStock = userStock;
@@ -62,66 +84,29 @@ namespace CashManager.Tests.AcceptanceCriteria.Transactions.Edit
             transactionVm.SaveTransactionCommand.Execute(null);
 
             app.SelectViewModelCommand.Execute(ViewModel.Search);
-            var searchVm = (SearchViewModel) app.SelectedViewModel;
-            searchVm.TransactionsListViewModel.SelectedTransaction = transaction;
-            Assert.True(searchVm.TransactionsListViewModel.TransactionEditCommand.CanExecute(null));
+            var searchVm = (SearchViewModel)app.SelectedViewModel;
+
+            //edit
+            searchVm.TransactionsListViewModel.SelectedTransaction = searchVm.TransactionsListViewModel.Transactions.FirstOrDefault(x => x.Id == id);
             searchVm.TransactionsListViewModel.TransactionEditCommand.Execute(null);
 
-            app.TransactionViewModel.Value.Transaction.Type = descType;
-
             //when
-            //todo: check if need: for some reason there is still search vm as "selected" instead of transaction vm...
-            app.TransactionViewModel.Value.SaveTransactionCommand.Execute(null);
+                //todo: for some reason there is still search vm as "selected" instead of transaction vm...
+                app.SelectViewModelCommand.Execute(ViewModel.Transaction);
+                transactionVm = (TransactionViewModel)app.SelectedViewModel;
+                transactionVm.Transaction = app.TransactionViewModel.Value.TransactionsProvider.AllTransactions.FirstOrDefault(x => x.Id == id);
+                //todo: 3 lines above should be removed, when search test will be added and problem will be fixed.
+            transactionVm.Transaction.Type = descType;
+            transactionVm.Transaction.Positions[0].Value.GrossValue = newValue;
+            Assert.True(transactionVm.IsInEditMode);
+            transactionVm.SetUpdateMode(TransactionEditModes.ChangeStockBalance);
+            transactionVm.SaveTransactionCommand.Execute(null);
 
             //then
-            Assert.Equal(2, app.TransactionViewModel.Value.TransactionsProvider.AllTransactions.Count);
+            Assert.Equal(2, app.TransactionViewModel.Value.TransactionsProvider.AllTransactions.Count); //one is from stock creation
             Assert.Equal(2, searchVm.TransactionsListViewModel.Transactions.Count);
-            Assert.Equal(expectedBalance, _fixture.Container.Resolve<StocksViewModel>().Stocks[0].UserBalance);
-        }
-
-        private TransactionType CreateType(TransactionTypes type)
-        {
-            var app = _fixture.Container.Resolve<ApplicationViewModel>();
-            app.SelectViewModelCommand.Execute(ViewModel.TypesManager);
-            var vm = (TransactionTypesViewModel) app.SelectedViewModel;
-            vm.AddTransactionTypeCommand.Execute(null);
-            var transactionType = vm.TransactionTypes[0];
-
-            switch (type)
-            {
-                case TransactionTypes.Income:
-                    transactionType.Income = true;
-                    transactionType.Name = "income";
-                    break;
-                case TransactionTypes.Outcome:
-                    transactionType.Outcome = true;
-                    transactionType.Name = "outcome";
-                    break;
-            }
-
-            return transactionType;
-        }
-
-        private Stock CreateUserStock(decimal balanceValue, int daysSinceLastStockEdit)
-        {
-            var app = _fixture.Container.Resolve<ApplicationViewModel>();
-            app.SelectViewModelCommand.Execute(ViewModel.StockManager);
-            var stockVm = (StocksViewModel) app.SelectedViewModel;
-            stockVm.AddStockCommand.Execute(null);
-            var userStock = stockVm.Stocks[0];
-            userStock.IsUserStock = true;
-            userStock.Name = "User stock no 1";
-            userStock.Balance.Value = balanceValue;
-
-            if (daysSinceLastStockEdit != 0)
-            {
-                userStock.Balance
-                         .GetType()
-                         .GetProperty("LastEditDate")?
-                         .SetValue(userStock.Balance, DateTime.Today.AddDays(-daysSinceLastStockEdit));
-            }
-
-            return userStock;
+            var stocksViewModel = _fixture.Container.Resolve<StocksViewModel>();
+            Assert.Equal(expectedBalance, stocksViewModel.Stocks[0].UserBalance);
         }
     }
 }
