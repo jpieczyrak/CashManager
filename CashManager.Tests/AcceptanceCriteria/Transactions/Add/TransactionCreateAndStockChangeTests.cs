1e840b4591e9be41b204de4ee216db7207594759
Jejek <jedrekpieczy@gmail.com>
2019-01-22 20:32:55 +0100

    Acceptance tests #3

--- CashManager.Tests/AcceptanceCriteria/Transactions/Add/TransactionCreateAndStockChangeTests.cs
+++ CashManager.Tests/AcceptanceCriteria/Transactions/Add/TransactionCreateAndStockChangeTests.cs
@@ -18,34 +18,24 @@ namespace CashManager.Tests.AcceptanceCriteria.Transactions.Add
         public TransactionCreateAndStockChangeTests(CleanableDatabaseFixture fixture) : base(fixture) { }
 
         [Theory]
-        [InlineData(1000, 500, TransactionTypes.Income, -1, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 0, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 1, 0, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, -1, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 0, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 1, -5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, -1, 5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 0, 5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Income, 1, 5, 1500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, -1, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 0, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 1, 0, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, -1, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 0, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 1, -5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, -1, 5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 0, 5, 500)]
-        [InlineData(1000, 500, TransactionTypes.Outcome, 1, 5, 500)]
-        public void AddingTransactionShouldModifyStockBalance(decimal startBalance, decimal transactionValue, TransactionTypes transactionType, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
+        [InlineData(1000, 500, -1, 0, 1500)]
+        [InlineData(1000, 500, 0, 0, 1500)]
+        [InlineData(1000, 500, 1, 0, 1500)]
+        [InlineData(1000, 500, -1, -5, 1500)]
+        [InlineData(1000, 500, 0, -5, 1500)]
+        [InlineData(1000, 500, 1, -5, 1500)]
+        [InlineData(1000, 500, -1, 5, 1500)]
+        [InlineData(1000, 500, 0, 5, 1500)]
+        [InlineData(1000, 500, 1, 5, 1500)]
+        public void AddingIncomeTransaction_ModifyBalance_StockBalanceShouldBeModified(decimal startBalance, decimal transactionValue, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
         {
-            //given
             var userStock = CreateUserStock(startBalance, daysSinceLastStockEdit);
-            var type = CreateType(transactionType);
+            var type = CreateType(TransactionTypes.Income);
             var app = _fixture.Container.Resolve<ApplicationViewModel>();
             app.SelectViewModelCommand.Execute(ViewModel.Transaction);
-            var transactionVm = (TransactionViewModel) app.SelectedViewModel;
+            var transactionVm = (TransactionViewModel)app.SelectedViewModel;
 
-            transactionVm.Modes[TransactionEditModes.ChangeStockBalance].IsSelected = true;
+            transactionVm.SetUpdateMode(TransactionEditModes.ChangeStockBalance);
             transactionVm.Transaction.Title = "first one";
             transactionVm.Transaction.Type = type;
             transactionVm.Transaction.UserStock = userStock;
@@ -62,19 +52,19 @@ namespace CashManager.Tests.AcceptanceCriteria.Transactions.Add
         }
 
         [Theory]
-        [InlineData(1000, 500, -1, 0, 1500)]
-        [InlineData(1000, 500, 0, 0, 1500)]
-        [InlineData(1000, 500, 1, 0, 1500)]
-        [InlineData(1000, 500, -1, -5, 1500)]
-        [InlineData(1000, 500, 0, -5, 1500)]
-        [InlineData(1000, 500, 1, -5, 1500)]
-        [InlineData(1000, 500, -1, 5, 1500)]
-        [InlineData(1000, 500, 0, 5, 1500)]
-        [InlineData(1000, 500, 1, 5, 1500)]
-        public void AddingIncomeTransaction_ModifyBalance_StockBalanceShouldBeModified(decimal startBalance, decimal transactionValue, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
+        [InlineData(1000, 500, -1, 0, 500)]
+        [InlineData(1000, 500, 0, 0, 500)]
+        [InlineData(1000, 500, 1, 0, 500)]
+        [InlineData(1000, 500, -1, -5, 500)]
+        [InlineData(1000, 500, 0, -5, 500)]
+        [InlineData(1000, 500, 1, -5, 500)]
+        [InlineData(1000, 500, -1, 5, 500)]
+        [InlineData(1000, 500, 0, 5, 500)]
+        [InlineData(1000, 500, 1, 5, 500)]
+        public void AddingOutcomeTransaction_ModifyBalance_StockBalanceShouldBeModified(decimal startBalance, decimal transactionValue, int daysSinceLastStockEdit, int transactionBookDateAsDaysCountUntilToday, decimal expectedBalance)
         {
             var userStock = CreateUserStock(startBalance, daysSinceLastStockEdit);
-            var type = CreateType(TransactionTypes.Income);
+            var type = CreateType(TransactionTypes.Outcome);
             var app = _fixture.Container.Resolve<ApplicationViewModel>();
             app.SelectViewModelCommand.Execute(ViewModel.Transaction);
             var transactionVm = (TransactionViewModel)app.SelectedViewModel;
