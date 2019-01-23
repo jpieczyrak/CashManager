dbf246b27f100937a61d1da51e2525aa36756731
Jejek <jedrekpieczy@gmail.com>
2019-01-23 15:07:18 +0100

    Ux

--- CashManager-MVVM/Properties/AssemblyInfo.cs
+++ CashManager-MVVM/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using log4net.Config;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.14.0")]
+[assembly: AssemblyVersion("0.8.15.0")]
 #if DEBUG
 [assembly: XmlConfigurator(ConfigFile = "log4net.Debug.config", Watch = true)]
 #else
