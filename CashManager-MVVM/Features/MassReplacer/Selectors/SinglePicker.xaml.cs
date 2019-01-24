b2e29cafd5d646f9967ad97cf90653aa942441c4
Jejek <jedrekpieczy@gmail.com>
2019-01-24 22:05:01 +0100

    Singlepicker

---
+++ CashManager-MVVM/Features/MassReplacer/Selectors/SinglePicker.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CashManager_MVVM.Features.MassReplacer.Selectors
+{
+    public partial class SinglePicker : UserControl
+    {
+        public SinglePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
