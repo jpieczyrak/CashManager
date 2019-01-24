46f971f34dbd3ec973f81d94498ab6fa61ca8346
Jejek <jedrekpieczy@gmail.com>
2019-01-24 21:53:23 +0100

    Value range selector

---
+++ CashManager-MVVM/Features/Search/Selectors/ValueRangeSelector.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CashManager_MVVM.Features.Search.Selectors
+{
+    public partial class ValueRangeSelector : UserControl
+    {
+        public ValueRangeSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
