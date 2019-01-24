578c8a21daeb6df1b8e7ba8c9272e9871e2abfba
Jejek <jedrekpieczy@gmail.com>
2019-01-24 22:01:37 +0100

    Multipicker

---
+++ CashManager-MVVM/Features/Search/Selectors/MultiPicker.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CashManager_MVVM.Features.Search.Selectors
+{
+    public partial class MultiPicker : UserControl
+    {
+        public MultiPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
