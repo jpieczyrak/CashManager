4cb833adc6815c7e78d1cfb89cbcf515c8924518
Jejek <jedrekpieczy@gmail.com>
2019-01-25 15:41:52 +0100

    Text setter - replace

---
+++ CashManager-MVVM/Logic/Commands/Setters/TextSetterCommand.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CashManager_MVVM.Model;
+using CashManager_MVVM.Model.Setters;
+
+namespace CashManager_MVVM.Logic.Commands.Setters
+{
+    public class TextSetterCommand : ISetter<Transaction>, ISetter<Position>
+    {
+        private readonly TextSetter _textSetter;
+
+        private TextSetterCommand(TextSetter textSetter)
+        {
+            _textSetter = textSetter;
+        }
+
+        #region ISetter<Position>
+
+        public IEnumerable<Position> Execute(IEnumerable<Position> elements)
+        {
+            var action = GetPositionAction(_textSetter.Type);
+            foreach (var position in elements) action(position, _textSetter.Value);
+            return elements;
+        }
+
+        #endregion
+
+        #region ISetter<Transaction>
+
+        public bool CanExecute() => _textSetter.IsChecked;
+
+        public IEnumerable<Transaction> Execute(IEnumerable<Transaction> elements)
+        {
+            var action = GetTransactionAction(_textSetter.Type);
+            foreach (var transaction in elements) action(transaction, _textSetter.Value);
+            return elements;
+        }
+
+        #endregion
+
+        public static TextSetterCommand Create(TextSetter setter)
+        {
+            switch (setter.Type)
+            {
+                case TextSetterType.Title:
+                    return new TextSetterCommand(setter);
+                case TextSetterType.Note:
+                    break;
+                case TextSetterType.PositionTitle:
+                    break;
+            }
+
+            return null;
+        }
+
+        private Action<Transaction, string> GetTransactionAction(TextSetterType type)
+        {
+            switch (type)
+            {
+                case TextSetterType.Title:
+                    return (transaction, s) => transaction.Title = s;
+                case TextSetterType.Note:
+                    break;
+                case TextSetterType.PositionTitle:
+                    break;
+            }
+
+            return null;
+        }
+
+        private Action<Position, string> GetPositionAction(TextSetterType type)
+        {
+            switch (type)
+            {
+                case TextSetterType.Title:
+                    return (position, s) => position.Title = s;
+                case TextSetterType.Note:
+                    break;
+                case TextSetterType.PositionTitle:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CashManager.Tests/MVVM.Logic/Setters/TransactionTextSetter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+using AutoMapper;
+
+using CashManager_MVVM.Logic.Commands.Setters;
+using CashManager_MVVM.Model;
+using CashManager_MVVM.Model.Setters;
+
+using Xunit;
+
+using MapperConfiguration = CashManager_MVVM.Configuration.Mapping.MapperConfiguration;
+
+namespace CashManager.Tests.MVVM.Logic.Setters
+{
+    public class TransactionTextSetter
+    {
+        private readonly Transaction[] _transactions =
+        {
+            new Transaction { Title="Title 1" },
+            new Transaction { Title="Title 2" },
+            new Transaction { Title="Title 3" }
+        };
+
+        [Fact]
+        public void TextSetter_DisabledSetter_NoChange()
+        {
+            //given
+            var textSetter = new TextSetter(TextSetterType.Title);
+            var command = TextSetterCommand.Create(textSetter);
+
+            //when
+            var result = command.Execute(_transactions);
+
+            //then
+            Assert.Equal(_transactions, result);
+        }
+
+        [Fact]
+        public void TextSetter_EnabledSetter_Change()
+        {
+            //given
+            MapperConfiguration.Configure();
+            string targetText = "title";
+            var textSetter = new TextSetter(TextSetterType.Title) { IsChecked = true, Value = targetText };
+            var command = TextSetterCommand.Create(textSetter);
+
+            //when
+            var result = command.Execute(_transactions);
+
+            //then
+            Assert.All(result.Select(x => x.Title), value => value.Equals(targetText));
+            var expected = Mapper.Map<Transaction[]>(Mapper.Map<CashManager.Data.DTO.Transaction[]>(_transactions));
+            foreach (var transaction in expected) transaction.Title = targetText;
+            Assert.Equal(expected, result);
+        }
+    }
+}
