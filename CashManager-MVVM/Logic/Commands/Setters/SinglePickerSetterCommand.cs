96df3ccb675e67c0b9a640b773b1e0449a4752a5
Jejek <jedrekpieczy@gmail.com>
2019-01-26 01:58:57 +0100

    Single picker setter

---
+++ CashManager-MVVM/Logic/Commands/Setters/SinglePickerSetterCommand.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CashManager_MVVM.Model;
+using CashManager_MVVM.Model.Common;
+using CashManager_MVVM.Model.Selectors;
+using CashManager_MVVM.Model.Setters;
+
+namespace CashManager_MVVM.Logic.Commands.Setters
+{
+    public class SinglePickerSetterCommand : ISetter<Transaction>, ISetter<Position>
+    {
+        private readonly SinglePicker _selector;
+
+        private SinglePickerSetterCommand(SinglePicker selector) { _selector = selector; }
+
+        #region ISetter<Transaction>
+
+        public bool CanExecute() => _selector?.IsChecked == true && _selector.Selected?.Value != null;
+
+        #endregion
+
+        public IEnumerable<Position> Execute(IEnumerable<Position> elements)
+        {
+            if (!CanExecute()) return elements;
+            if (_selector.Type == MultiPickerType.Category)
+            {
+                var setter = GetPositionAction();
+                foreach (var position in elements) setter(position, _selector.Selected.Value);
+            }
+            else
+            {
+                Execute(elements.Select(x => x.Parent).Distinct());
+            }
+            return elements;
+        }
+
+        public IEnumerable<Transaction> Execute(IEnumerable<Transaction> elements)
+        {
+            if (!CanExecute()) return elements;
+            var setter = GetTransactionAction();
+            foreach (var transaction in elements) setter(transaction, _selector.Selected.Value);
+            return elements;
+        }
+
+        public static SinglePickerSetterCommand Create(SinglePicker selector) => new SinglePickerSetterCommand(selector);
+
+        private Action<Transaction, BaseObservableObject> GetTransactionAction()
+        {
+            switch (_selector.Type)
+            {
+                case MultiPickerType.Category:
+                    return (transaction, o) =>
+                    {
+                        foreach (var position in transaction.Positions)
+                            position.Category = o as Category;
+                    };
+                case MultiPickerType.UserStock:
+                    return (transaction, o) => transaction.UserStock = o as Stock;
+                case MultiPickerType.ExternalStock:
+                    return (transaction, o) => transaction.ExternalStock = o as Stock;
+                case MultiPickerType.TransactionType:
+                    return (transaction, o) => transaction.Type = o as TransactionType;
+            }
+
+            return null;
+        }
+
+        private Action<Position, BaseObservableObject> GetPositionAction()
+        {
+            switch (_selector.Type)
+            {
+                case MultiPickerType.Category:
+                    return (position, o) => position.Category = o as Category;
+                case MultiPickerType.UserStock:
+                    return (position, o) => position.Parent.UserStock = o as Stock;
+                case MultiPickerType.ExternalStock:
+                    return (position, o) => position.Parent.ExternalStock = o as Stock;
+                case MultiPickerType.TransactionType:
+                    return (position, o) => position.Parent.Type = o as TransactionType;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CashManager.Tests/MVVM.Logic/Setters/Transactions/SinglePicker/SinglePickerSetter.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+
+using CashManager_MVVM.Logic.Commands.Setters;
+using CashManager_MVVM.Model;
+using CashManager_MVVM.Model.Common;
+using CashManager_MVVM.Model.Selectors;
+
+using Xunit;
+
+namespace CashManager.Tests.MVVM.Logic.Setters.Transactions.SinglePicker
+{
+    public class SinglePickerSetter
+    {
+        private readonly TransactionType _typeA = new TransactionType { Name = "A" };
+        private readonly TransactionType _typeB = new TransactionType { Name = "B" };
+
+        [Fact]
+        public void DateSetter_DisabledSetter_NoChange()
+        {
+            //given
+            var selector = new CashManager_MVVM.Model.Selectors.SinglePicker(MultiPickerType.TransactionType, GetTransactionTypes());
+            var command = SinglePickerSetterCommand.Create(selector);
+            var transactions = GetTransactions();
+
+            //when
+            var result = command.Execute(transactions);
+
+            //then
+            Assert.Equal(transactions, result);
+        }
+
+        [Fact]
+        public void DateSetter_EnabledSetter_Change()
+        {
+            //given
+            var selector = new CashManager_MVVM.Model.Selectors.SinglePicker(MultiPickerType.TransactionType, GetTransactionTypes())
+            {
+                IsChecked = true,
+                Selected = new Selectable(_typeB)
+            };
+            var command = SinglePickerSetterCommand.Create(selector);
+            var transactions = GetTransactions();
+
+            //when
+            var result = command.Execute(transactions);
+
+            //then
+            Assert.All(result.Select(x => x.Type), type => Assert.Equal(type, _typeB));
+        }
+
+        private Transaction[] GetTransactions()
+        {
+            return new[]
+            {
+                new Transaction { Type = _typeA },
+                new Transaction { Type = _typeA },
+                new Transaction { Type = _typeA }
+            };
+        }
+
+        private Selectable[] GetTransactionTypes() { return new[] { _typeA, _typeB }.Select(x => new Selectable(x)).ToArray(); }
+    }
+}
