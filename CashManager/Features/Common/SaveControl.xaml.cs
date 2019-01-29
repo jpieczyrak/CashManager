324f255c19f2052d6c7ec38059327f81432a7877
Jejek <jedrekpieczy@gmail.com>
2019-01-29 16:47:31 +0100

    Save icon sketch

---
+++ CashManager/Features/Common/SaveControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CashManager.Features.Common
+{
+    public partial class SaveControl : UserControl
+    {
+        public SaveControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
