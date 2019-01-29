782ed729d1bcdaddc3bb1fe6b2bf3d7f0cff4891
Jejek <jedrekpieczy@gmail.com>
2019-01-29 13:18:24 +0100

    Custom csv parser

--- CashManager/Features/Parsers/Custom/CsvParserViewModel.cs
+++ CashManager/Features/Parsers/Custom/CsvParserViewModel.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Linq;
+
+using AutoMapper;
 
 using CashManager.CommonData;
 using CashManager.Infrastructure.Command;
@@ -6,12 +9,17 @@ using CashManager.Infrastructure.Query;
 using CashManager.Logic.Parsers;
 using CashManager.Logic.Parsers.Custom;
 
+using GalaSoft.MvvmLight.CommandWpf;
+
 using DtoStock = CashManager.Data.DTO.Stock;
 
 namespace CashManager.Features.Parsers.Custom
 {
     public class CsvParserViewModel : ParserViewModelBase
     {
+        public RelayCommand AddRuleCommand { get; }
+        public RelayCommand<Model.Parsers.Rule> RemoveRuleCommand { get; }
+
         private TrulyObservableCollection<Model.Parsers.Rule> _rules;
 
         public TrulyObservableCollection<Model.Parsers.Rule> Rules
@@ -24,11 +32,24 @@ namespace CashManager.Features.Parsers.Custom
             }
         }
 
+        public TransactionField[] AvailableProperties
+            => Enum.GetValues(typeof(TransactionField)).OfType<TransactionField>().ToArray();
+
         public CsvParserViewModel(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher, TransactionsProvider transactionsProvider) : base(queryDispatcher,
             commandDispatcher, transactionsProvider)
         {
             Parser = new RegexParser();
-            Rules = new TrulyObservableCollection<Model.Parsers.Rule>();
+            Rules = new TrulyObservableCollection<Model.Parsers.Rule>
+            {
+                new Model.Parsers.Rule { Column = 1, Property = TransactionField.Title, IsOptional = false }
+            };
+            Rules.CollectionChanged += (sender, args) => Parser = new CustomCsvParser(Mapper.Map<Rule[]>(_rules), Mapper.Map<DtoStock[]>(UserStocks));
+            AddRuleCommand = new RelayCommand(() => Rules.Add(new Model.Parsers.Rule
+            {
+                Column = 1,
+                Property = AvailableProperties.Except(Rules.Select(x => x.Property)).FirstOrDefault()
+            }));
+            RemoveRuleCommand = new RelayCommand<Model.Parsers.Rule>(x => Rules.Remove(x));
         }
     }
 }
