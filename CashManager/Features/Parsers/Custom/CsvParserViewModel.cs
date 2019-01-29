cb33d7582c940058593910e9b04b2eafd347b098
Jejek <jedrekpieczy@gmail.com>
2019-01-29 23:52:16 +0100

    Save control

--- CashManager/Features/Parsers/Custom/CsvParserViewModel.cs
+++ CashManager/Features/Parsers/Custom/CsvParserViewModel.cs
@@ -6,23 +6,26 @@ using AutoMapper;
 using CashManager.CommonData;
 using CashManager.Infrastructure.Command;
 using CashManager.Infrastructure.Query;
-using CashManager.Logic.Parsers;
 using CashManager.Logic.Parsers.Custom;
 
 using GalaSoft.MvvmLight.CommandWpf;
 
 using DtoStock = CashManager.Data.DTO.Stock;
+using Rule = CashManager.Model.Parsers.Rule;
 
 namespace CashManager.Features.Parsers.Custom
 {
     public class CsvParserViewModel : ParserViewModelBase
     {
+        private TrulyObservableCollection<Rule> _rules;
+
+        private string _elementSplitter;
+
         public RelayCommand AddRuleCommand { get; }
-        public RelayCommand<Model.Parsers.Rule> RemoveRuleCommand { get; }
 
-        private TrulyObservableCollection<Model.Parsers.Rule> _rules;
+        public RelayCommand<Rule> RemoveRuleCommand { get; }
 
-        public TrulyObservableCollection<Model.Parsers.Rule> Rules
+        public TrulyObservableCollection<Rule> Rules
         {
             get => _rules;
             set
@@ -31,8 +34,6 @@ namespace CashManager.Features.Parsers.Custom
                 UpdateParser();
             }
         }
-
-        private string _elementSplitter;
 
         public string ElementSplitter
         {
@@ -44,27 +45,32 @@ namespace CashManager.Features.Parsers.Custom
             }
         }
 
-        public TransactionField[] AvailableProperties
-            => Enum.GetValues(typeof(TransactionField)).OfType<TransactionField>().ToArray();
+        public TransactionField[] AvailableProperties => Enum.GetValues(typeof(TransactionField)).OfType<TransactionField>().ToArray();
+
+        public RelayCommand<string> ParserSaveCommand { get; }
 
         public CsvParserViewModel(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher, TransactionsProvider transactionsProvider) : base(queryDispatcher,
             commandDispatcher, transactionsProvider)
         {
             _elementSplitter = ";";
-            Rules = new TrulyObservableCollection<Model.Parsers.Rule>
+            Rules = new TrulyObservableCollection<Rule>
             {
-                new Model.Parsers.Rule { Column = 1, Property = TransactionField.Title, IsOptional = false }
+                new Rule { Column = 1, Property = TransactionField.Title, IsOptional = false }
             };
             Rules.CollectionChanged += (sender, args) => UpdateParser();
-            AddRuleCommand = new RelayCommand(() => Rules.Add(new Model.Parsers.Rule
+            AddRuleCommand = new RelayCommand(() => Rules.Add(new Rule
             {
                 Column = 1,
                 Property = AvailableProperties.Except(Rules.Select(x => x.Property)).FirstOrDefault()
             }));
-            RemoveRuleCommand = new RelayCommand<Model.Parsers.Rule>(x => Rules.Remove(x));
+            RemoveRuleCommand = new RelayCommand<Rule>(x => Rules.Remove(x));
+            ParserSaveCommand = new RelayCommand<string>(x =>
+            {
+                //todo save
+                Console.WriteLine(x);
+            }, x => !string.IsNullOrWhiteSpace(x));
         }
 
-        private void UpdateParser()
-            => Parser = new CustomCsvParser(Mapper.Map<Rule[]>(_rules), Mapper.Map<DtoStock[]>(UserStocks), ElementSplitter);
+        private void UpdateParser() => Parser = new CustomCsvParser(Mapper.Map<Logic.Parsers.Custom.Rule[]>(_rules), Mapper.Map<DtoStock[]>(UserStocks), ElementSplitter);
     }
 }
