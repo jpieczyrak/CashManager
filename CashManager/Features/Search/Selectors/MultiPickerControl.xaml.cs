68bd90241579e011e0fab7c79d2d8d447ba4d10f
Jejek <jedrekpieczy@gmail.com>
2019-01-30 19:17:42 +0100

    Ux

--- CashManager/Features/Search/Selectors/MultiPickerControl.xaml.cs
+++ CashManager/Features/Search/Selectors/MultiPickerControl.xaml.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace CashManager.Features.Search.Selectors
 {
     public partial class MultiPickerControl : UserControl
     {
+        public static readonly DependencyProperty BorderThicknessProperty = DependencyProperty.Register(
+            nameof(BorderThickness), typeof(Thickness), typeof(MultiPickerControl), new PropertyMetadata(default(Thickness)));
+
+        public Thickness BorderThickness
+        {
+            get => (Thickness) GetValue(BorderThicknessProperty);
+            set => SetValue(BorderThicknessProperty, value);
+        }
         public MultiPickerControl()
         {
             InitializeComponent();
