7693de01a0d8fa69b45bb1b077e047a3c85e30fe
Jejek <jedrekpieczy@gmail.com>
2019-02-09 01:34:36 +0100

    Datepicker - date converter

---
+++ CashManager/Converters/DateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CashManager.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        #region IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).ToString("dd.MM.yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DateTime.TryParse((string)value, out DateTime date) ? date : DateTime.MinValue;
+        }
+
+        #endregion
+    }
+}
