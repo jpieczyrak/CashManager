509be13f6da875f8a71048bf9ae29668bf113bf0
Jejek <jedrekpieczy@gmail.com>
2019-03-03 14:49:10 +0100

    Saving bill back to PC

--- CashManager/Features/Transactions/Bills/BillWindow.xaml.cs
+++ CashManager/Features/Transactions/Bills/BillWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using CashManager.Features.Common;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+
+using CashManager.Features.Common;
+
+using Microsoft.Win32;
 
 namespace CashManager.Features.Transactions.Bills
 {
@@ -8,6 +15,19 @@ namespace CashManager.Features.Transactions.Bills
         {
             InitializeComponent();
             DataContext = image;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new SaveFileDialog
+            {
+                AddExtension = true,
+                DefaultExt = ".jpg",
+                Filter = "JPG (.jpg)|*.jpg"
+            };
+
+            if (fileDialog.ShowDialog() == true)
+                Image.FromStream(new MemoryStream(DataContext as byte[])).Save(fileDialog.FileName, ImageFormat.Jpeg);
         }
     }
 }
