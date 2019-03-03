8d2d2993dc6e3c8c1abda2cffe17b339b59467a1
Jejek <jedrekpieczy@gmail.com>
2019-03-03 14:02:33 +0100

    Cleanup

--- CashManager/Features/Categories/CategoryManagerViewModel.cs
+++ CashManager/Features/Categories/CategoryManagerViewModel.cs
@@ -68,7 +68,6 @@ namespace CashManager.Features.Categories
 
         public CategoryManagerViewModel(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher, IMessagesService messagesService)
         {
-            commandDispatcher.Execute(new UpsertCategoriesCommand(DtoCategory.Default)); //todo: remove after beta!!!
             _input = Strings.NewCategory;
             _commandDispatcher = commandDispatcher;
             _messagesService = messagesService;
