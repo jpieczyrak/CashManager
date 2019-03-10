01267d88b418f2a840a38da8f130d3113933f225
Jejek <jedrekpieczy@gmail.com>
2019-03-10 19:24:00 +0100

    Build scrypt

--- CashManager/Properties/AssemblyInfo.cs
+++ CashManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using log4net.Config;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 #if DEBUG
 [assembly: XmlConfigurator(ConfigFile = "log4net.Debug.config", Watch = true)]
 #elif PORTABLE
---
+++ build.ps1
@@ -0,0 +1,30 @@
+﻿cls
+cd $PSScriptRoot
+
+$OutDirSetup64 = "..\Builds\Release-64";
+$OutDirPortable64 = "..\Builds\Portable-64";
+$OutDirPortable32 = "..\Builds\Portable-32";
+$Releases = $PSScriptRoot + "\Releases\";
+
+
+&$Env:msbuild /p:Configuration=Portable /p:Platform="Any CPU" /p:DebugSymbols=false /p:OutputPath=$OutDirPortable32
+&$Env:msbuild /p:Configuration=Portable /p:Platform=x64 /p:DebugSymbols=false /p:OutputPath=$OutDirPortable64
+&$Env:msbuild /p:Configuration=Release /p:Platform=x64 /p:DebugSymbols=false /p:OutputPath=$OutDirSetup64
+
+$PSScriptRoot + "/CashManager/" + $OutDirPortable32
+cd $PSScriptRoot/CashManager/$OutDirPortable32
+&$Env:7z a ./../portable-32.7z *.*
+
+$PSScriptRoot + "/CashManager/" + $OutDirPortable64
+cd $PSScriptRoot/CashManager/$OutDirPortable64
+&$Env:7z a ./../portable-64.7z *.*
+
+
+$Assembly = [Reflection.Assembly]::Loadfile($PSScriptRoot +"/CashManager/" + $OutDirPortable64 + '/CashManager.exe')
+$AssemblyName = $Assembly.GetName()
+$Version = $AssemblyName.version
+$Version.ToString(3)
+
+Copy-Item ($PSScriptRoot + "/Builds/portable-64.7z") -Destination ($Releases + 'portable-' + $Version.ToString(3) + '.7z');
+Copy-Item ($PSScriptRoot + "/Builds/portable-32.7z") -Destination ($Releases + 'portable-' + $Version.ToString(3) + '-32bit.7z');
+Copy-Item ($Releases + "Setup.exe") -Destination ($Releases + 'Setup-' + $Version.ToString(3) + '.exe');
